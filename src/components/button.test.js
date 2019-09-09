4d18e60aa9e2328919e73278f4c1678b4b3dbc63
Ben Grabowski <gbowski30@gmail.com>
2019-09-08 19:48:15 -0600

    ready to test

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-
+import toJson from 'enzyme-to-json';
 import Button from './button';
 
 describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
-        const wrapper = shallow(<Button />);
+        const wrapper = shallow(<Button onClick={props.onClick} />);
         wrapper.find('button').simulate('click');
+        expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
